238b8c2a85b553626fa1b70db898983370814820
JordonGiusto <jordon.giusto@gmail.com>
2024-06-21 21:52:28 -0700

    moivment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float maxAcceleration, maxSpeed;
+    public float maxAcceleration, maxSpeed, crouchSpeed;
 
 
     public Sniperjam sj;
@@ -18,8 +18,12 @@ public class PlayerController : MonoBehaviour
 
     public float mouseSense;
 
+
+    Animator animator;
+
     public Camera cam;
 
+    bool crouched;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +31,17 @@ public class PlayerController : MonoBehaviour
         sj =  new Sniperjam();
         sj.Enable();
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         currentSpeed = Vector3.zero;
 
         sj.Player.Move.performed += handleMoveInput;
         sj.Player.Move.canceled += handleMoveInput;
         sj.Player.Look.performed += handleLookInput;
         sj.Player.Look.canceled += handleLookInput;
+
+        sj.Player.Crouch.performed += handleCrouchInput;
+        sj.Player.Crouch.canceled += handleCrouchInput;
+
 
         Cursor.lockState = CursorLockMode.Locked;
 
@@ -71,7 +80,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 transformedInput = transform.TransformDirection(new Vector3(currentMoveInput.x, 0, currentMoveInput.y));
 
-        Vector2 targetVelo = new Vector2(transformedInput.x, transformedInput.z) * maxSpeed;
+        Vector2 targetVelo = new Vector2(transformedInput.x, transformedInput.z) * (crouched ? crouchSpeed : maxSpeed);
 
         Vector2 v2CurrentSpeed = new Vector2(currentSpeed.x, currentSpeed.z);
         Vector2 acceleration = Vector2.ClampMagnitude(targetVelo - v2CurrentSpeed, maxAcceleration * Time.deltaTime);
@@ -93,5 +102,20 @@ public class PlayerController : MonoBehaviour
     public void handleLookInput(InputAction.CallbackContext context)
     {
         currentLookInput = context.ReadValue<Vector2>();
+    }
+
+    public void handleCrouchInput(InputAction.CallbackContext context)
+    {
+        
+        if (context.performed)
+        {
+            animator.SetTrigger("Crouch");
+            crouched = true;
+        }
+        else
+        {
+            animator.SetTrigger("Uncrouch");
+            crouched = false;
+        }
     }
 }
